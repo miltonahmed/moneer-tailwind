2068ab6fbe095ebd0b357b5160b3ac0cfde75e46
Milton Ahmed <miltonahmed08@yahoo.com>
2025-07-27 18:44:02 +0400

    feat: Navbar-responsive
    - add custom breakpoint
    -add bars icon
    -responsive device ul-(hidden)
    -add padding responsive device

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,15 +1,43 @@
-import Container from "../../Utils/Container";
+import { FaBars, FaUser } from 'react-icons/fa';
+import { Link, NavLink } from 'react-router';
+import Logo from '../../assets/images/logo.png';
+import Container from '../../Utils/Container';
+import { menuData } from '../../Utils/Data/menuData';
+import Image from '../../Utils/Image';
+
 
 const Navbar = () => {
   return (
     <>
-      <nav>
+      <nav className=" py-3 sm:py-4  lg:py-6 bg-navBg">
         <Container>
-          <h1>Milton Ahmed</h1>
+          <div className="flex justify-between items-center xs:mx-4 md:mx-6 xl:mx-auto">
+            {/* Logo  */}
+            <Link to="/">
+              <Image source={Logo} alt="company logo" />
+            </Link>
+            {/* Menu  */}
+            <ul className=" hidden lg:flex items-center gap-14">
+              {menuData.map(item => (
+                <li key={item.id}>
+                  {/* Menu link  */}
+                  <NavLink
+                    to={item.path}
+                    className="text-white text-base font-bold font-lato capitalize leading-6 tracking-[0.7px] relative after:content-[''] after:absolute after:bottom-[-10px] after:left-0 after:w-full after:h-[2px] after:transition-transform after:duration-300 hover:after:scale-x-100 after:scale-x-0 navItemHover hover:font-black"
+                  >
+                    {item.name}
+                  </NavLink>
+                </li>
+              ))}
+              <FaUser className="text-white text-base" /> 
+            </ul>
+            {/* Bars icons  */}
+            <FaBars className='text-white text-2xl lg:hidden'/>
+          </div>
         </Container>
       </nav>
     </>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
